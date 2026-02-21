812fc38c29304c1062cfec5960fbd77b9d129dee
Nikita Rjaško <github@nktrjsk.cz>
2026-02-21 22:32:04 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Python dev environment with uv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            uv
+          ];
+
+          shellHook = ''
+            echo "uv $(uv --version)"
+          '';
+        };
+      });
+}
